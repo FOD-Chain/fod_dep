4bacbfb1206b6dd1c67e07b3e0120d954d6fae78
399677477@qq.com <fcq@fortunes.com.cn>
2018-09-14 19:35:25 +0800

    all

---
+++ public/js/main.js
@@ -0,0 +1,543 @@
+var i18n = function(){};
+i18n.cn = {
+	"contract":"智能合约",
+	"overview":"详情",
+	"balance":"数量",
+	"mined":"挖出",
+	"transactions":"交易",
+	"internal_transactions":"内部交易",
+	"contract_source":"合约源码",
+	"tx_hash":"交易Hash",
+	"block":"块",
+	"from":"从",
+	"to":"到",
+	"gas":"gas",
+	"age":"时间",
+	"height":"高度",
+	"prev":"前",
+	"next":"后",
+	"time":"时间",
+	"ago":"之前",
+	"uncles":"叔块",
+	"Uncles":"叔块",
+	"Hash":"哈希",
+	"parentHash":"父哈希",
+	"sha3_Uncles":"叔块sha3",
+	"Mined_By":"矿工",
+	"Difficulty":"难度",
+	"Total_Difficulty":"总难度",
+	"Gas_Limit":"Gas限制",
+	"Gas_Used":"已用Gas",
+	"nonce":"序号",
+	"Extra_Data":"附加数据",
+	"Source Code":"源码",
+	"Bytecode and ABI":"字节码和ABI",
+	"Verify and Publish Contract Source Code":"验证并发布合约代码",
+	"Enter Contract Source Code below.":"在下面输入合约代码",
+	"If the compiled bytecode matches the":"如果编译出的字节码符合",
+	"Creation Address":"创建地址",
+	"bytecode, the contract is then":"的字节码，合约将",
+	"Verified":"通过认证",
+	"and will be published online.":"然后上传到网络。",
+	"Contract Address":"合约地址",
+	"Contract Name":"合约名称",
+	"Optimization Enabled":"优化开启",
+	"Contract Code: ":"合约代码：",
+	"Constructor Arguments ABI-encoded (OPTIONAL - Required for certain contracts only):":"构造集合ABI编码（可选 - 仅在确认合约使用）：",
+	"Validate Code":"合法代码",
+	"Reset":"重置",
+	"Contract Source Code Verified":"合约代码认证",
+	"View the verified result at":"查看认证结果",
+	"Unable to Verify Code":"无法认证合约代码",
+	"The compiled result does not match the input creation bytecode located at":"编译结果无法匹配输入的创建字节码于",
+	"Compiler Version:":"编译器版本：",
+	"Optimization Enabled:":"优化开启：",
+	"ContractName:":"合约名称：",
+	"ContractBytecode:":"合约字节码：",
+	"ContractABI:":"合约ABI：",
+	"Start Over":"重新开始",
+	"Contract Source Code Unverified":"合约代码未验证",
+	"Verify And Publish Source Code.":"验证并发布源码",
+	"Contract Name:":"合约名字：",
+	"Contract Source Code":"合约源码",
+	"Amount":"数量",
+	"Created":"创建",
+	"Newer":"新",
+	"Older":"旧",
+	"blockNumber":"块高度",
+	"creates":"创造",
+	"value":"数量",
+	"gas Provided":"提供gas",
+	"gasPrice":"gas单价",
+	"Input Data":"附加数据",
+	"Blocks":"块",
+	"Block Height":"块高度",
+	"Block Time":"出块时间",
+	"Network Hashrate":"网络哈希速率",
+	"DIFFICULTY":"难度",
+	"Address Balance":"地址币量",
+	"Total":"总共",
+	"Token Holders":"Token持有者",
+	"Contract Creator":"合约创建者",
+	"Token Address":"Token地址",
+	"Get Balance":"获取币量",
+	"Token Transfers":"Token交易",
+	"Contract Transactions":"合约交易",
+	"Contract Source":"合约源码",
+	"Get":"获取",
+	"Enter Your Address":"输入你的邮箱",
+	"You have":"你有",
+	"Transfer":"交易",
+	"MENU":"菜单",
+	"Home":"主页",
+	"View All Tokens":"查看所有Tokens",
+	"View Contracts (N/A)":"查看合约 (N/A)",
+	"Stats":"状态",
+	"HashRate Growth Chart":"哈希速率增长图",
+	"Miner Distribution Chart":"矿工分配图",
+	"Difficulty Chart":"难度图",
+	"Blocktime Chart":"出块时间图",
+	"The bomb chart":"难度炸弹图",
+	"Language":"语言",
+	"14 day Fodcreate Transaction History": "14天Fodcreate交易历史",
+};
+i18n.en = {
+	"contract":"Contract",
+	"overview":"Overview",
+	"balance":"Balance",
+	"mined":"Mined",
+	"transactions":"Transactions",
+	"internal_transactions":"Internal Transactions",
+	"contract_source":"Contract Source",
+	"tx_hash":"TxHash",
+	"block":"Block",
+	"from":"From",
+	"to":"To",
+	"gas":"gas",
+	"age":"Age",
+	"height":"height",
+	"prev":"Prev",
+	"next":"Next",
+	"time":"Time",
+	"ago":"ago",
+	"uncles":"uncles",
+	"Uncles":"Uncles",
+	"Hash":"Hash",
+	"parentHash":"parentHash",
+	"sha3_Uncles":"sha3 Uncles",
+	"Mined_By":"Mined By",
+	"Difficulty":"Difficulty",
+	"Total_Difficulty":"Total Difficulty",
+	"Gas_Limit":"Gas Limit",
+	"Gas_Used":"Gas Used",
+	"nonce":"nonce",
+	"Extra_Data":"Extra Data",
+	"Source Code":"Source Code",
+	"Bytecode and ABI":"Bytecode and ABI",
+	"Verify and Publish Contract Source Code":"Verify and Publish Contract Source Code",
+	"Enter Contract Source Code below.":"Enter Contract Source Code below.",
+	"If the compiled bytecode matches the":"If the compiled bytecode matches the",
+	"Creation Address":"Creation Address",
+	"bytecode, the contract is then":"bytecode, the contract is then",
+	"Verified":"Verified",
+	"and will be published online.":"and will be published online",
+	"Contract Address":"Contract Address",
+	"Contract Name":"Contract Name",
+	"Optimization Enabled":"Optimization Enabled",
+	"Contract Code: ":"Contract Code: ：",
+	"Constructor Arguments ABI-encoded (OPTIONAL - Required for certain contracts only):":"Constructor Arguments ABI-encoded (OPTIONAL - Required for certain contracts only):",
+	"Validate Code":"Validate Code",
+	"Reset":"Reset",
+	"Contract Source Code Verified":"Contract Source Code Verified",
+	"View the verified result at":"View the verified result at",
+	"Unable to Verify Code":"Unable to Verify Code",
+	"The compiled result does not match the input creation bytecode located at":"The compiled result does not match the input creation bytecode located at",
+	"Compiler Version:":"Compiler Version:",
+	"Optimization Enabled:":"Optimization Enabled:",
+	"ContractName:":"ContractName:",
+	"ContractBytecode:":"ContractBytecode:",
+	"ContractABI:":"ContractABI",
+	"Start Over":"Start Over",
+	"Contract Source Code Unverified":"Contract Source Code Unverified",
+	"Verify And Publish Source Code.":"Verify And Publish Source Code.",
+	"Contract Name:":"Contract Name:",
+	"Contract Source Code":"Contract Source Code",
+	"Contract ABI":"Contract ABI",
+	"Contract Bytecode":"Contract Bytecode",
+	"Amount":"Amount",
+	"Created":"Created",
+	"Newer":"Newer",
+	"Older":"Older",
+	"blockNumber":"blockNumber",
+	"creates":"creates",
+	"value":"value",
+	"gas Provided":"gas Provided",
+	"gasPrice":"gasPrice",
+	"Input Data":"Input Data",
+	"Blocks":"Blocks",
+	"Block Height":"Block Height",
+	"Block Time":"Block Time",
+	"Network Hashrate":"Network Hashrate",
+	"DIFFICULTY":"DIFFICULTY",
+	"Address Balance":"Address Balance",
+	"Total":"Total",
+	"Token Holders":"Token Holders",
+	"Contract Creator":"Contract Creator",
+	"Token Address":"Token Address",
+	"Get Balance":"Get Balance",
+	"Token Transfers":"Token Transfers",
+	"Contract Transactions":"Contract Transactions",
+	"Contract Source":"Contract Source",
+	"Get":"Get",
+	"Enter Your Address":"Enter Your Address",
+	"You have":"You have",
+	"Transfer":"Transfer",
+	"MENU":"MENU",
+	"Home":"Home",
+	"View All Tokens":"View All Tokens",
+	"View Contracts (N/A)":"View Contracts (N/A)",
+	"Stats":"Stats",
+	"HashRate Growth Chart":"HashRate Growth Chart",
+	"Miner Distribution Chart":"Miner Distribution Chart",
+	"Difficulty Chart":"Difficulty Chart",
+	"Blocktime Chart":"Blocktime Chart",
+	"The bomb chart":"The bomb chart",
+	"Language":"Language",
+	"14 day Fodcreate Transaction History": "14 day Fodcreate Transaction History",
+};
+i18n.get = function(index){
+	var language = localStorage.getItem("Language");
+	language = language ? language : "cn";
+	return i18n[language][index];
+}
+i18n.setLanguage = function(language){
+	localStorage.setItem("Language", language);
+}
+
+
+var BlocksApp = angular.module("BlocksApp", [
+    "ui.router", 
+    "ui.bootstrap", 
+    "oc.lazyLoad",  
+    "ngSanitize"
+]); 
+BlocksApp.constant('_', window._); // loadsh
+BlocksApp.config(['$ocLazyLoadProvider',  '$locationProvider', 
+    function($ocLazyLoadProvider, $locationProvider) {
+    $ocLazyLoadProvider.config({
+        cssFilesInsertBefore: 'ng_load_plugins_before' // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+    });
+    $locationProvider.html5Mode({
+      enabled: true
+    });
+}]);
+/* Setup global settings */
+BlocksApp.factory('settings', ['$rootScope', '$http', function($rootScope, $http) {
+    // supported languages
+    var settings = {
+        layout: {
+            pageSidebarClosed: false, // sidebar menu state
+            pageContentWhite: false, // set page content layout
+            pageBodySolid: false, // solid body color state
+            pageAutoScrollOnLoad: 1000 // auto scroll to top on page load
+        },
+        assetsPath: '/',
+        globalPath: '/',
+        layoutPath: '/',
+    };
+
+    $rootScope.settings = settings;
+    return settings;
+}]);
+
+/* Load config settings */
+BlocksApp.factory('setupObj', ['$rootScope', '$http', function($rootScope, $http) {
+    return $http.get('/config').then(function(res) {
+        return res.data;
+    })
+}]);
+
+/* Setup App Main Controller */
+BlocksApp.controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.$on('$viewContentLoaded', function() {
+        //App.initComponents(); // init core components
+        //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive 
+    });
+}]);
+
+/***
+Layout Partials.
+By default the partials are loaded through AngularJS ng-include directive.
+***/
+/* Setup Layout Part - Header */
+BlocksApp.controller('HeaderController', ['$scope', '$location', 'setupObj', function($scope, $location, setupObj) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initHeader(); // init header
+    });
+    $scope.form = {};
+    $scope.searchQuery = function(s) {
+        var search = s.toLowerCase();
+
+        $scope.form.searchInput="";
+        $scope.form.searchForm.$setPristine();
+        $scope.form.searchForm.$setUntouched();
+        if (isAddress(search)) 
+            $location.path("/addr/" + search);
+        else if (isTransaction(search))
+            $location.path("/tx/" + search);
+        else if (!isNaN(search))
+            $location.path("/block/" + search);
+        else 
+            $scope.form.searchInput = search;
+    }
+    setupObj.then(function(res) {
+        $scope.settings = res;
+    });
+}]);
+/* Search Bar */
+BlocksApp.controller('PageHeadController', ['$scope', 'setupObj', function($scope, setupObj) {
+    $scope.$on('$includeContentLoaded', function() {        
+        
+    });
+    setupObj.then(function(res) {
+        $scope.settings = res;
+    });
+}]);
+/* Setup Layout Part - Footer */
+BlocksApp.controller('FooterController', ['$scope', 'setupObj', function($scope, setupObj) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initFooter(); // init footer
+    });
+    setupObj.then(function(res) {
+        $scope.settings = res;
+    });
+}]);
+/* Setup Rounting For All Pages */
+BlocksApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    // Redirect any unmatched url
+    $urlRouterProvider.otherwise("home");      
+    $stateProvider
+        // Dashboard
+        .state('home', {
+            url: "/home",
+            templateUrl: "views/home.html",            
+            data: {pageTitle: 'Blockchain Explorer'},
+            controller: "HomeController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load([{
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                            '/js/controllers/HomeController.js',
+                            '/css/todo-2.min.css'
+                        ]}]);
+                }]
+            }
+        })
+        .state('address', {
+            url: "/addr/{hash}",
+            templateUrl: "views/address.html",
+            data: {pageTitle: 'Address'},
+            controller: "AddressController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                             '/js/controllers/AddressController.js',
+                            '/plugins/datatables/datatables.min.css',
+                            '/plugins/datatables/datatables.bootstrap.css',
+                            '/plugins/datatables/datatables.all.min.js',
+                            '/plugins/datatables/datatable.min.js'
+                        ]
+                    });
+                }]
+            }
+        })
+        .state('block', {
+            url: "/block/{number}",
+            templateUrl: "views/block.html",
+            data: {pageTitle: 'Block'},
+            controller: "BlockController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                             '/js/controllers/BlockController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+
+        .state('uncle', {
+            url: "/uncle/*number",
+            templateUrl: "views/block.html",
+            data: {pageTitle: 'Uncle'},
+            controller: "UncleController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                             '/js/controllers/UncleController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+
+        .state('tx', {
+            url: "/tx/{hash}",
+            templateUrl: "views/tx.html",
+            data: {pageTitle: 'Transaction'},
+            controller: "TxController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                             '/js/controllers/TxController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+        .state('contract', {
+            url: "/contract/{addr}",
+            templateUrl: "views/contract.html",
+            data: {pageTitle: 'Verify Contract'},
+            controller: "ContractController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                             '/js/controllers/ContractController.js',
+                             '/js/custom.js'
+                         ]
+                     });
+                }]
+            }
+        })
+        .state('stats', {
+            url: "/stats/{chart}",
+            templateUrl: "views/stats/index.html",
+            data: {pageTitle: 'Statistics'},
+            controller: "StatsController",
+            resolve: {
+                deps: ['$ocLazyLoad', '$stateParams', function($ocLazyLoad, $stateParams) {
+                    return $ocLazyLoad.load({
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                             '/js/controllers/StatsController.js',
+                             '/css/stats.css',
+                             "https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.10/d3.js",
+                             "/plugins/async.min.js",
+                             "/plugins/moment/moment.min.js"
+                        ]
+                    });
+                }]
+            }
+        })
+        .state('tokenlist', {
+            url: "/token",
+            templateUrl: "views/tokenlist.html",
+            data: {pageTitle: 'Tokens'},
+            controller: "TokenListController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                             '/js/controllers/TokenListController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+        .state('token', {
+            url: "/token/{hash}",
+            templateUrl: "views/token.html",
+            data: {pageTitle: 'Tokens'},
+            controller: "TokenController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                             '/js/controllers/TokenController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+        .state('dao', {
+            url: "/dao",
+            templateUrl: "views/dao.html",
+            data: {pageTitle: 'theDAO'},
+            controller: "DAOController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                             '/js/controllers/DAOController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+        .state('err404', {
+            url: "/err404/{thing}/{hash}",
+            templateUrl: "views/err_404.html",
+            data: {pageTitle: '404 Not Found.'},
+            controller: "ErrController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        name: 'BlocksApp',
+                        insertBefore: '#ng_load_plugins_before', 
+                        files: [
+                             '/js/controllers/ErrController.js'
+                        ]
+                    });
+                }]
+            }
+        })
+}]);
+BlocksApp.filter('timeDuration', function() {
+  return function(timestamp) {
+    return getDuration(timestamp).toString();
+  };
+})
+.filter('totalDifficulty', function() {
+  return function(hashes) {
+    return getDifficulty(hashes);
+  };
+}) 
+.filter('teraHashes', function() {
+    return function(hashes) {
+        var result = hashes / Math.pow(1000, 4);
+        return parseInt(result);
+  }
+})
+/* Init global settings and run the app */
+BlocksApp.run(["$rootScope", "settings", "$state", "setupObj", function($rootScope, settings, $state, setupObj) {
+    $rootScope.$state = $state; // state to be accessed from view
+    $rootScope.$settings = settings; // state to be accessed from view
+    setupObj.then(function(res) {
+    	 res.i18n=i18n;
+        $rootScope.setup = res;
+    });
+}]);
